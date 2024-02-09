11eabf84c352994d5c5cb818012319af1319cd69
dc5116 <dc5116@nyu.edu>
2024-02-08 22:19:35 -0500

    update batch

--- batch_training.sh
+++ batch_training.sh
@@ -2,7 +2,7 @@
 
 for depth in 2 4 6 8
 do
-    for width_factor in 16 32
+    for width_factor in 16 32 64
     do
         sbatch training.slurm $depth $width_factor;
         echo "Submitted job for depth = $depth, width_factor = $width_factor"

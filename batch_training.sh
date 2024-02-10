bc535f32e79dc9668c64d2fd510b1ea152d56593
dc5116 <dc5116@nyu.edu>
2024-02-10 15:54:03 -0500

    add train

--- batch_training.sh
+++ batch_training.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for depth in 2 4 6 8 10 
+for depth in 2 3 4
 do
-    for width_factor in 16 32 64
+    for width_factor in 128 256 512
     do
         sbatch training.slurm $depth $width_factor;
         echo "Submitted job for depth = $depth, width_factor = $width_factor"
